80f64eb7eadfa2e70ff3a3189e8d64893d71fcc3
Eduardo Olivares <eduardo.olivares@sjsu.edu>
2018-05-13 12:14:58 -0700

    Removed footer. Decreased timer size .

--- js/date-time.js
+++ js/date-time.js
@@ -3,7 +3,7 @@ var index = 0;
 
 function Timer() {
     var d = new Date();
-    document.getElementById("time").innerHTML = "<div class='col-md-12'><h1 class='text-center'>" + d.toLocaleDateString() + " "  + d.toLocaleTimeString() + "</h1></div>";
+    document.getElementById("time").innerHTML = "<div class='col-md-12'><h2 class='text-center'><strong>" + d.toLocaleDateString() + " "  + d.toLocaleTimeString() + "</strong></h2></div>";
     var s = d.getSeconds();
     if (s % 5 == 0 || s ==  0 ){
     index += 1
